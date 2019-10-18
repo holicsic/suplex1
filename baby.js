4ea367e9bc78f627d347105952c56befe6027f39
Evans Obafemi Aiyenuro <kushy@Evanss-MacBook-Pro.local>
2019-10-18 16:35:42 +0200

    exe

--- baby.js
+++ baby.js
@@ -1,2 +1,12 @@
-var x = 12;
-console.log('Hello world' );
+var x = "John"
+var y = "Doe"
+console.log(x +" "+ y)
+
+var details = {firstName:"joey",lasName:"moore",email:"joeymoro@aol.com",age:50};
+
+delete details.email;
+
+var gadget = ["iphone","toshiba","samsung","sharp","aloe","breakPad","spiner","speaker","300","-2"];
+
+console.log(gadget)
+
